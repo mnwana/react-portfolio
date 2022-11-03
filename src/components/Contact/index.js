6aeebbb0c2909299c9e87edc0e5bb06fb7c29be7
mnwana <mariellenwana@gmail.com>
2022-11-02 23:53:57 -0400

    add contact me form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-function Contact(){
-    return (
-        <section>
-
-        </section>
-    );
+function Contact() {
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name"></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="emai;"></input>
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5"></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
 export default Contact;
