deb701e30cd1da518e11d20ffc6cc299d1e08dec
mnwana <mariellenwana@gmail.com>
2022-11-02 23:44:55 -0400

    add about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
-function About(){
-    return (
-        <section>
-        </section>
-    );
+function About() {
+  return (
+    <section>
+      <p>
+        My name is Marielle Nwana and I'm an NYC-based data analyst.
+        <br></br><br></br>
+        Proven understanding of data analytics and engineering tools and
+        passionate about data accessibility and data literacy. Experience
+        includes leading projects within data teams, presenting data driven
+        insights to leadership, and partnering across engineering, business and
+        sales teams to transform raw data into data tools and dashboards for
+        internal and external measurement. Thrives in high-growth startup
+        environments where there is a good balance of autonomy and
+        collaboration.
+        <br></br><br></br>
+        When I'm not working, I enjoy trying out new recipes, playing with my
+        cat Molly, and exporing all that New York has to offer.
+      </p>
+    </section>
+  );
 }
 
 export default About;
