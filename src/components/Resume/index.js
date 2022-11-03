8eec8bac37fd310d9448e580a966a3ce54138de6
mnwana <mariellenwana@gmail.com>
2022-11-03 00:21:05 -0400

    add resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,23 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { Document,Page } from "react-pdf";
+import resumeFile from '../../assets/resume.pdf';
 function Resume() {
-  return <section></section>;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  return (
+    <section>
+    <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      <Page pageNumber={pageNumber} />
+    </Document>
+    <p>
+      Page {pageNumber} of {numPages}
+    </p>
+  </section>
+  );
 }
 
 export default Resume;
