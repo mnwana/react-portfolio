3806df516c162aa364f53c1dff3eff5978e49a12
mnwana <mariellenwana@gmail.com>
2022-11-03 19:56:33 -0400

    clean pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import resumeFile from "../../assets/resume.pdf";
 function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
+  function onDocumentLoadSuccess() {
   }
   return (
     <section>
