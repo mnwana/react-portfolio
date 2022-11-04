94ef28f3c750e7c351b871e8e6d840a912384bb6
mnwana <mariellenwana@gmail.com>
2022-11-04 01:46:11 -0400

    fix resume filename

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import resumeFile from "../../assets/resume.pdf";
 function Resume(props) {
-
-  const {pageSelected} = props;
-  if(pageSelected === 'resume'){
-  return (
-    <section className="col-md-6">
-      <iframe
-        title="Resume"
-        style={{ width: "100%", height: "100%" }}
-        src={resumeFile}
-      />
-      <button className="button" type="submit">
-      <a href={resumeFile} download>Download</a>
-      </button>
-    </section>
-  );}
+  const { pageSelected } = props;
+  if (pageSelected === "resume") {
+    return (
+      <section className="col-md-6">
+        <iframe
+          title="Resume"
+          style={{ width: "100%", height: "100%" }}
+          src={resumeFile}
+        />
+        <button className="button" type="submit">
+          <a href={resumeFile} download='MarielleNwanaResume.PDF'>
+            Download
+          </a>
+        </button>
+      </section>
+    );
+  }
   return;
 }
 
