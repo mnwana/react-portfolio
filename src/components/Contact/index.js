6864a861004c5457dcf3ba677f22744e6b5609c8
mnwana <mariellenwana@gmail.com>
2022-11-03 21:14:21 -0400

    add contact form handlers (and footer component)

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,28 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { validateEmail } from "../../utils/helpers";
 
 function Contact(props) {
-  const {pageSelected} = props;
-  if(pageSelected === 'contact'){
-  return (
-    <section>
-      <h1>Contact Me</h1>
-      <form>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name"></input>
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="emai;"></input>
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5"></textarea>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </section>
-  );
+  const { pageSelected } = props;
+
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log("Handle Form", formState);
+    }
+  };
+  // add functionality to send this to sql db
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log("Submit Form", formState);
+    } else {
+      console.log("Form Submitted!");
+    }
+  };
+
+  if (pageSelected === "contact") {
+    return (
+      <section>
+        <h1>Contact Me</h1>
+        <form>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name"></input>
+          </div>
+          <div>
+            <label htmlFor="email">Email address:</label>
+            <input type="email" name="emai;"></input>
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" rows="5"></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    );
   }
   return;
 }
