65028ad7578106a0c0816c9fb3c9906199f7e3e1
mnwana <mariellenwana@gmail.com>
2022-11-03 21:31:42 -0400

    add project screenshots

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -49,20 +49,25 @@ function Contact(props) {
     return (
       <section>
         <h1>Contact Me</h1>
-        <form>
+        <form id="contact-form" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name"></input>
+            <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
           </div>
           <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" name="emai;"></input>
+            <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
           </div>
           <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5"></textarea>
+            <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
           </div>
-          <button type="submit">Submit</button>
+          {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+          <button data-testid='button' type="submit">Submit</button>
         </form>
       </section>
     );
