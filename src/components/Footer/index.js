b0cd8826b048d30ff19562c09506140813f83dae
mnwana <mariellenwana@gmail.com>
2022-11-05 15:06:12 -0400

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 function Footer() {
-  return (<footer className="mt-4 justify-content-center">
+  return (<footer className="mt-4 justify-content-center bg-secondary footer fixed-bottom">
             <ul className="row justify-content-center text-center p-0">
           <li className="col-md-2 list-group-item">
-            <a href="mailto:mariellenwana@gmail.com"> Email </a>
+            <a href="mailto:mariellenwana@gmail.com" className="text-dark"> Email </a>
           </li>
           <li className="col-md-2 list-group-item">
-            <a href="https://www.github.com/mnwana"> GitHub </a>
+            <a href="https://www.github.com/mnwana" className="text-dark"> GitHub </a>
           </li>
           <li className="col-md-2 list-group-item">
-            <a href="https://www.linkedin.com/in/mnwana"> LinkedIn </a>
+            <a href="https://www.linkedin.com/in/mnwana" className="text-dark"> LinkedIn </a>
           </li>
           <li className="col-md-2 list-group-item">
-            <a href="https://www.instagram.com/marntech"> Instagram </a>
+            <a href="https://www.instagram.com/marntech" className="text-dark"> Instagram </a>
           </li>
         </ul>
   </footer> );
