4aa7b71ed2ca7bcc2662c0f50b64dfade718eca6
mnwana <mariellenwana@gmail.com>
2023-01-12 22:41:14 -0500

    add gtm tag

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Nav from "./components/Nav";
 import Resume from "./components/Resume";
+import TagManager from 'react-gtm-module'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const tagManagerArgs = {
+    gtmId: 'GTM-N6QXHVV'
+}
+TagManager.initialize(tagManagerArgs)
+
   const [pageSelected, setCurrentPage] = useState( 'about');
   return (
     <div className="flex mb-2">
