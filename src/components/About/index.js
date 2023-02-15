3f38c985d13c84e84386fd06c12a75df3dea036a
Marielle Nwana <mariellenwana@gmail.com>
2023-02-15 12:27:04 -0500

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -19,14 +19,12 @@ function About(props) {
         <div className="text-center border w-s-100 mnBorderPrimary mnBGLight rounded p-2 col-md-6">
           <p> I'm Marielle Nwana and I'm an NYC-based Data Analyst.</p>
           <p>
-            I have a proven understanding of data analytics and engineering
-            tools and passion for data accessibility and data literacy. My
-            experience includes leading projects within data teams, presenting
-            data driven insights to leadership, and partnering across
-            engineering, client and sales teams to transform raw data into data
-            tools and dashboards for internal and external measurement. I thrive
-            in high-growth startup environments where there is a good balance of
-            collboration, autonomy, and contiuous learning.
+            I have a proven understanding of data analytics and engineering tools and am an advocate 
+            for data accessibility and data literacy. My experience includes leading projects within 
+            data teams from data ingestion to analysis, presenting data driven insights to leadership, 
+            and partnering across engineering, business and sales teams to transform raw data into data 
+            tools and dashboards for internal and external measurement. I thrive in high-growth startup 
+            environments where there is a healthy balance of collaboration and autonomy.
           </p>
           <p>
             When I'm not working, I enjoy trying out new recipes, playing with
