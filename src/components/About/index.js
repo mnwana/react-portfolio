82acc4fd7c10ebfdaa14d3d2c104f658b2de2e11
mnwana <mariellenwana@gmail.com>
2023-02-28 00:11:44 -0500

    clean up about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,37 +1,44 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function About(props) {
   const { pageSelected } = props;
   if (pageSelected === "about") {
     return (
-      <section className="col-sm-10 col-md-8 align-center mx-auto">
+      <section className="align-center mx-auto">
         <h1 className="text-center mnTextSecondary">About</h1>
-        <div className="row flex-row">
-        <img
-                  alt="Marielle Nwana"
-                  src={
-                    require(`../../assets/images/self-photo.jpeg`).default
-                  }
-                  id="self-photo"
-                  className="img mnBorderPrimary border rounded center col-md-6 p-0"
-                />
-        {/* change about me to be a code style block */}
-        <div className="text-center border w-s-100 mnBorderPrimary mnBGLight rounded p-2 col-md-6">
-          <p> I'm Marielle Nwana and I'm an NYC-based Data Analyst.</p>
-          <p>
-            I have a proven understanding of data analytics and engineering tools and am an advocate 
-            for data accessibility and data literacy. My experience includes leading projects within 
-            data teams from data ingestion to analysis, presenting data driven insights to leadership, 
-            and partnering across engineering, business and sales teams to transform raw data into data 
-            tools and dashboards for internal and external measurement. I thrive in high-growth startup 
-            environments where there is a healthy balance of collaboration and autonomy.
-          </p>
-          <p>
-            When I'm not working, I enjoy trying out new recipes, playing with
-            my cat Molly, and exporing all that the Big Apple has to offer.
-          </p>
-        </div>
-        </div>
+        <Row className="d-flex justify-content-center">
+          <Col xs={6} md={4}>
+            <Image
+              alt="Marielle Nwana"
+              src={require(`../../assets/images/self-photo.jpeg`).default}
+              id="self-photo"
+              rounded
+              fluid
+            />
+          </Col>
+          <Col xs={6} md={5}>
+            <p> I'm Marielle Nwana and I'm an NYC-based Data Analyst.</p>
+            <p>
+              I have a proven understanding of data analytics and engineering
+              tools and am an advocate for data accessibility and data literacy.
+              My experience includes leading projects within data teams from
+              data ingestion to analysis, presenting data driven insights to
+              leadership, and partnering across engineering, business and sales
+              teams to transform raw data into data tools and dashboards for
+              internal and external measurement. I thrive in high-growth startup
+              environments where there is a healthy balance of collaboration and
+              autonomy.
+            </p>
+            <p>
+              When I'm not working, I enjoy trying out new recipes, playing with
+              my cat Molly, and exporing all that the Big Apple has to offer.
+            </p>
+          </Col>
+        </Row>
       </section>
     );
   }
